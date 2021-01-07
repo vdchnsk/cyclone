5c2668cdf7703d479d28e60a07040ff2e6e029ff
Valeriy Dichanskiy <dichansky@gmail.com>
2021-01-07 18:32:37 +0300

    added some functional/fixed bugs/changed interface

--- src/App.js
+++ src/App.js
@@ -19,24 +19,27 @@ class  App extends React.Component {
     e.preventDefault(); //Уничтожение обыкновенного поведения страницы,при отправики формы - обновления
     let city = e.target.elements.city.value;//получаем значение введенное пользователем в инпут
 
-    if(city ) { //city именно из API,не cityName
-      const api_url = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);//апи и его фетч
+    if(city ) { //city ,что ввел пользователь
+      let punctuationless = city.replace(/[.,<>/#!$%^&*;:{}=\-_`~()]/g,"");//Удаляем всю пунктуацию/пробелы и пр.из введенного названия города. P.s.:Дико извиняюсь за нейминг,худшего придумать было просто нельзя
+      let ExtraSimpolnessCityName = punctuationless.replace(/\s{2,}/g,"");
+      // console.log(ExtraSimpolnessCityName)
+      const api_url = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${ExtraSimpolnessCityName}&appid=${API_KEY}&units=metric`);//апи и его фетч,получаем ссылку конеретно для введенного города
       const data = await api_url.json();
-      
-      let sunset = data.sys.sunset;
-      let date = new Date();
-      date.setTime(sunset);
-      let sunset_date = date.getHours() + "hr:" + date.getMinutes() + "mn:" + date.getSeconds() + "s";
-
-      this.setState({
-        temp:data.main.temp,
-        cityName:data.name,
-        countryName:data.sys.country,
-        pressure:data.main.pressure,
-        sunset:sunset_date,
-        error:undefined
-      });
-    } else {
+      if(data.cod!=="404"){ //Если ссылка не выдает "404",то все норм и код выполняется.Хотя консоль выдает ошибку,не смотря на то,что React не жалуется,error вывести пока не получается,но все же лучше ,чем ошибка на всю страницу 
+        let sunset = data.sys.sunset;
+        let date = new Date();
+        date.setTime(sunset);
+        let sunset_date = date.getHours() + "hr:" + date.getMinutes() + "mn:" + date.getSeconds() + "s";
+        this.setState({
+          temp:data.main.temp,
+          cityName:data.name,
+          countryName:data.sys.country,
+          pressure:data.main.pressure,
+          sunset:sunset_date,
+          error:undefined
+        });
+      }
+  } else if(!city){
       this.setState({
         temp:undefined,
         cityName:undefined,
@@ -46,8 +49,9 @@ class  App extends React.Component {
         error:"Введите название города"
       });
     }
+    
   }
-  render(){
+  render(){ //Рендерим компоненты и выводим все ,что получили из API
     return (
       <div className="App">
         {/* bg */}
--- src/app_window/weatherInfo.jsx
+++ src/app_window/weatherInfo.jsx
@@ -4,12 +4,12 @@ class Weather extends React.Component{
         return(
             <div className="weatherInfoTab">
                 {this.props.cityName && //ПРоверка на наличие значений
-                <div>
-                    <p>Местоположение:{this.props.cityName}, {this.props.countryName}</p>
-                    <p>Температура:{this.props.temp} °C</p>
-                    <p>Давление:{this.props.pressure} мм рт.ст.</p>
-                    <p>Заход солнца:{this.props.sunset}</p>
-                </div>
+                <div className="weatherInfoTab__outputTab-general">
+                    <div className="weatherInfoTab__outputTab-secondary"><p className="weatherInfoTab__ouputTab-text">Местоположение:{this.props.cityName}, {this.props.countryName} </p> </div>
+                    <div className="weatherInfoTab__outputTab-secondary"><p className="weatherInfoTab__ouputTab-text">Температура:{this.props.temp} °C </p> </div>
+                    <div className="weatherInfoTab__outputTab-secondary"><p className="weatherInfoTab__ouputTab-text">Давление:{this.props.pressure} мм рт.ст.</p> </div>
+                    <div className="weatherInfoTab__outputTab-secondary"><p className="weatherInfoTab__ouputTab-text">Заход солнца:{this.props.sunset} </p> </div>
+                </div> 
                 }
                 <p>{this.props.error}</p>
             </div>
