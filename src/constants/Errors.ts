4a65deb41f2b3b46016f84702a42af826a8a3bdc
Valeriy Dichanskiy <dichansky@gmail.com>
2022-02-12 16:15:03 +0300

    Improvement: Modified error statements

--- src/constants/Errors.ts
+++ src/constants/Errors.ts
@@ -1,2 +1,2 @@
-export const CITY_IS_NOT_CHOSEN = 'Chose the city!'
-export const CITY_IS_NOT_FOUND = 'Chosen is not found :('
+export const CITY_IS_NOT_CHOSEN = 'Chosoe the city!'
+export const CITY_IS_NOT_FOUND = 'Chosen city is not found :('
