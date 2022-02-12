71548db4b9e18bd69ffb3334edbeb4d548666370
Valeriy Dichanskiy <dichansky@gmail.com>
2022-02-12 15:56:14 +0300

    Design: Added and set new icons for sections and search input

--- src/components/mainWindow/weatherInfo.tsx
+++ src/components/mainWindow/weatherInfo.tsx
@@ -11,20 +11,44 @@ const WeatherTab = ({ weatherData }: { weatherData: WeatherDataElement }): JSX.E
       {weatherData.name && (
         <div className="weatherInfoTab__outputTab-general">
           <div className="weatherInfoTab__outputTab-secondary">
+            <img
+              src="../../../public/assets/icons/buildings_icon.png"
+              width={64}
+              height={64}
+              alt="buildings illustration"
+            />
             <h3 className="weatherInfoTab__ouputTab-text">Place:</h3>
             <p className="weatherInfoTab__ouputTab-text">
               {weatherData?.name}, {weatherData?.sys?.country}
             </p>
           </div>
           <div className="weatherInfoTab__outputTab-secondary">
+            <img
+              src="../../../public/assets/icons/thermometer_icon.png"
+              width={64}
+              height={64}
+              alt="thermometer illustration"
+            />
             <h3 className="weatherInfoTab__ouputTab-text">Temperature:</h3>
             <p>{weatherData?.main?.temp} °C</p>
           </div>
           <div className="weatherInfoTab__outputTab-secondary">
+            <img
+              src="../../../public/assets/icons/preassure_icon.png"
+              width={64}
+              height={64}
+              alt="preassure illustration"
+            />
             <h3 className="weatherInfoTab__ouputTab-text">Preassure:</h3>
             <p className="weatherInfoTab__ouputTab-text">{weatherData?.main?.pressure} Pa</p>
           </div>
           <div className="weatherInfoTab__outputTab-secondary">
+            <img
+              src="../../../public/assets/icons/sunset_icon.png"
+              width={64}
+              height={64}
+              alt="sunset illustration"
+            />
             <h3 className="weatherInfoTab__ouputTab-text">Sunset:</h3>
             <p className="weatherInfoTab__ouputTab-text">{weatherData.sunset}</p>
           </div>
